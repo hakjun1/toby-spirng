5db981e3edf86fdec74319a8fba5baa1c65dc6fa
Hakjun <hiatus9014@gmail.com>
2022-10-25 11:09:45 +0900

    factory context.getBean

--- src/main/java/com/likelion/dao/UserDaoFactory.java
+++ src/main/java/com/likelion/dao/UserDaoFactory.java
@@ -1,12 +1,17 @@
 package com.likelion.dao;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class UserDaoFactory {
 
+    @Bean
     public UserDao awsUserDao() {
         UserDao userDao = new UserDao(new AwsConnectionMaker());
         return userDao;
     }
-
+    @Bean
     public UserDao localUserDao() {
         UserDao userDao = new UserDao(new LocalConnectionMaker());
         return userDao;
--- src/test/java/com/likelion/dao/UserDaoTest.java
+++ src/test/java/com/likelion/dao/UserDaoTest.java
@@ -2,16 +2,25 @@ package com.likelion.dao;
 
 import com.likelion.domain.User;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = UserDaoFactory.class)
 class UserDaoTest {
+
+    @Autowired
+    ApplicationContext context;
 
     @Test
     void addAndget() throws SQLException {
-        UserDao userDao = new UserDaoFactory().localUserDao();
+        UserDao userDao = context.getBean("localUserDao",UserDao.class);
         userDao.deleteAll();
         userDao.add(new User("1","hakjun","1234"));
         User user = userDao.get("1");
